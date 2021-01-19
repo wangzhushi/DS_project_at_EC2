40a83300a2686ef0bd0d7fe775e5130567c2c7fb
Jason Wang <wangzhushi@yahoo.com>
2021-01-18 20:17:06 -0500

    daily etl successful version

--- python_py/finnhub_etl.py
+++ python_py/finnhub_etl.py
@@ -1,208 +1,107 @@
-#!/usr/bin/python_64
-# -*- coding: utf-8 -*-
-import sybpydb
-import sys
-import os
-import subprocess
+import pytz
 import datetime
-reload(sys)
-sys.setdefaultencoding('utf8')
-usage = "Usage:\n ./mmf_div_recon.py"
-#print str(os.getenv('DSQUERY'))
-#print str(os.getenv('SQL_USER'))
-env = str(os.getenv('DSQUERY')) 
-usr = str(os.getenv('SQL_USER'))
-out = subprocess.check_output(["echo", env + "." + usr])
-out = out.rstrip()
-
-con = sybpydb.connect(
-servername=str(os.getenv('DSQUERY')),
-user= str(os.getenv('SQL_USER')),
-password = subprocess.check_output(["secget", str(os.getenv('DSQUERY')) + "." + str(os.getenv('SQL_USER'))]).rstrip())
-
-chg = con.cursor()
-chained = "SET CHAINED ON"
-chg.execute(chained)
-
-cursor = con.cursor()
+from project_utils import db_util
+import csv as csv
+import sqlite3
+from sqlalchemy import create_engine
+import pandas
+import time
 
 
-q1 = "Select GETDATE()"
-cursor.execute(q1)
-rows = cursor.fetchall()
-res = rows[0]
-today_date = res[0]
-cursor.connection.commit()
-filepath = os.path.join(os.getenv('IW_WORK_OUT'), 'MMFDivRecon' + str(today_date.strftime('%m-%d-%y')) + ".csv")
-f = open(filepath, 'w')
-f.write(str(today_date.strftime('%m-%d-%y')) + ",                           CI Investments,                                   Page 1\n")
-f.write("                                                ,FUND DIVIDEND UPDATE\n                                              ,LIST OF EXCEPTIONS\n\t,\n")
-f.write("Parent_Fund_Code," + "Main_Fund_Code," +  "Fund_Name" + (" " * 16) + "," + "Unit_Distr_Cap," + "Unit_Distr_Income,"  + "T_Fund_Flag,"  +  "Corp_Trust_Code,"  + "Fund_Type" + "\n")
+def read_stock_list(file_name):
+    stk_list = list()
+    with open(file_name, 'r') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',')
+        for row in csvreader:
+            stk_list.append(row[0])
+    return stk_list
 
 
-q2 = "CREATE TABLE ##All_Distributions(Main_Fund_Code Fund_Code_Class, Unit_Distr_Cap Fractional_Dollar_Class NULL, Unit_Distr_Income Fractional_Dollar_Class NULL, Parent_Fund_Code Fund_Code_Class) "
-cur2 = con.cursor()
-cur2.execute(q2)
-cur2.connection.commit()
+def daily_etl(interval, start_time, end_time):
+    # stock_list = read_stock_list("../../postgresql/data/harry_sec_list_1000.csv")
+    stock_list = read_stock_list("harry_sec_list_1000.csv")
+    # stock_list = ['AAPL', 'TSLA']
+    # db = db_util.connect_to_db()
+    # engine = create_engine('sqlite://', echo=False)
+    engine = db_util.create_db_engine()
+    table = f"us_equity_{interval}_finnhub"
+    if interval == 'daily':
+        res = 'D'
+    elif interval == '1m':
+        res = '1'
 
-q3="""INSERT INTO ##All_Distributions
-SELECT DISTINCT a.Main_Fund_Code, Unit_Distr_Cap, Unit_Distr_Income, Parent_Fund_Code 
-FROM Fund_Div_Distr a, Fund_Map b, Fund c, System_Control sc
-WHERE b.Main_Fund_Code = a.Main_Fund_Code AND
-c.Main_Fund_Code = a.Main_Fund_Code AND
-a.Distr_Date = dbo.UNBD_udf(sc.Last_Posting_Date,1)  AND
-c.Fund_Type = 'MMF' AND
-c.Product_Type_Code = 'M'"""
-cur3 = con.cursor()
-cur3.execute(q3)
-cur3.connection.commit()
+    for each_stock in stock_list:
+        start = eastern_tz.localize(datetime.datetime.today())
+        es = each_stock
+        # es = 'AAPL'
+        raw_df = db_util.download_data_from_finnhub(es, res, start_time, end_time)
+        raw_df['symbol'] = es
+        raw_df['trade_date_int'] = [db_util.datetime_to_int_todate(datetime.datetime.fromtimestamp(x).date())
+                                    for x in raw_df['t']]
+        raw_df['t'] = pandas.to_datetime(raw_df['t'], unit='s')
 
-q4="CREATE TABLE ##PF_Codes(Parent_Fund_Code Fund_Code_Class, Main_Fund_Code Fund_Code_Class) "
-cur4 = con.cursor()
-cur4.execute(q4)
-cur4.connection.commit()
+        raw_df = raw_df.rename({'c': 'close_price', 'h': 'high_price', 'l': 'low_price', 'o': 'open_price',
+                                's': 'return_status', 't': 'trade_time', 'v': 'volume'}, axis='columns')
+#        raw_df = raw_df.reset_index()
+#         print(raw_df.head())
+#        raw_df.drop(columns='index')
 
-q5="""INSERT INTO ##PF_Codes
-SELECT DISTINCT b.Parent_Fund_Code, b.Main_Fund_Code   
-FROM Fund a, Fund_Map b
-WHERE a.Fund_Type = 'MMF' AND
-a.Product_Type_Code = 'M' AND
-a.Main_Fund_Code = b.Main_Fund_Code """
-cur5 = con.cursor()
-cur5.execute(q5)
-cur5.connection.commit()
+#        raw_df.to_sql(table, engine, index=False, chunksize=None, if_exists='append', method="multi")
+        raw_df.to_sql(table, engine, index=False, chunksize=None, if_exists='append', method="multi")
 
-q6="CREATE TABLE ##All_MFs(Parent_Fund_Code Fund_Code_Class, Main_Fund_Code Fund_Code_Class, Unit_Distr_Cap Fractional_Dollar_Class NULL, Unit_Distr_Income Fractional_Dollar_Class NULL)"
-cur6 = con.cursor()
-cur6.execute(q6)
-cur6.connection.commit()
+        now = eastern_tz.localize(datetime.datetime.today())
+        print('finish ' + each_stock + ' time: ' + str(now - start) + 'total time: '+ str(now - process_start_time) )
 
-q7="""INSERT INTO ##All_MFs
-SELECT a.Parent_Fund_Code, a.Main_Fund_Code, Unit_Distr_Cap, Unit_Distr_Income 
-FROM ##PF_Codes a, ##All_Distributions b WHERE 
-a.Main_Fund_Code *= b.Main_Fund_Code AND
-a.Parent_Fund_Code *= b.Parent_Fund_Code 
-ORDER BY Parent_Fund_Code"""
-cur7 = con.cursor()
-cur7.execute(q7)
-cur7.connection.commit()
-
-q8 = "CREATE TABLE ##Pending_Purch(Fund_Code Fund_Code_Class)"
-cur8 = con.cursor()
-cur8.execute(q8)
-cur8.connection.commit()
+    return True
 
 
-q9="""INSERT INTO ##Pending_Purch
-SELECT DISTINCT a.Fund_Code 
-FROM WO_Purch a, WO_Placement b, ##All_MFs c, Entry_Fund_Code d
-WHERE a.Fund_Code = d.Fund_Code AND
-d.Main_Fund_Code = c.Main_Fund_Code AND
-a.WO_No = b.WO_No AND
-b.WO_Type = 'P' AND
-b.Proc_Status = 'P'"""
-cur9 = con.cursor()
-cur9.execute(q9)
-cur9.connection.commit()
+if __name__ == '__main__':
+    # scope = input("please input running scope: ALL for everyday, One for one day, SP for specified day").upper()
+    # resolution = input("please input running interval: D for daily, M for 1 minute").upper()
+    scope = 'ALL'
+    resolution = 'D'
+    eastern_tz = pytz.timezone("US/Eastern")
+    process_start_time = eastern_tz.localize(datetime.datetime.today())
+    today = eastern_tz.localize(datetime.datetime.today()).date()
+    tomorrow = today + datetime.timedelta(days=1)
+    yesterday = today + datetime.timedelta(days=-1)
+    interval = None
+    base_time = datetime.datetime(1970, 1, 1).date()
+    init_mark_day = datetime.datetime(2010, 1, 1).date()
+    # cut_off_day = datetime.datetime(2001, 1, 1).date()
+    cut_off_day = today
 
+    if scope == 'ALL':
+        start_time = int((init_mark_day - base_time).total_seconds())
+        end_time = int((cut_off_day - base_time).total_seconds())
+        if resolution == "D":
+            interval = 'daily'
+        elif resolution == "M":
+            interval = '1m'
+        else:
+            print("don't support this resolution function yet")
 
-q10= "CREATE TABLE ##Temp_Output(Parent_Fund_Code Fund_Code_Class, Main_Fund_Code Fund_Code_Class, Fund_Name varchar(45), Unit_Distr_Cap Fractional_Dollar_Class NULL, Unit_Distr_Income Fractional_Dollar_Class NULL, Report varchar(2))"
-cur10 = con.cursor()
-cur10.execute(q10)
-cur10.connection.commit()
+    elif scope == 'ONE':
+        start_time = int((yesterday - base_time).total_seconds())
+        end_time = int((today - base_time).total_seconds())
+        if resolution == "D":
+            interval = 'daily'
+        elif resolution == "M":
+            interval = '1m'
+        else:
+            print("don't support this resolution function yet")
 
-q11="""INSERT INTO ##Temp_Output
-SELECT DISTINCT a.Parent_Fund_Code, a.Main_Fund_Code, d.Fund_Name, a.Unit_Distr_Cap, a.Unit_Distr_Income, '' as Report 
-FROM ##All_MFs a , Daily_Fund_Share_Balance b, ##Pending_Purch c, Fund d
-WHERE a.Main_Fund_Code = b.Main_Fund_Code AND
-a.Main_Fund_Code = d.Main_Fund_Code AND
-b.Posting_Date = (SELECT Last_Posting_Date FROM System_Control) AND
-((Units_NCI + Units_CI - Units_OSRED) > 0 OR a.Main_Fund_Code = c.Fund_Code)"""
-cur11 = con.cursor()
-cur11.execute(q11)
-cur11.connection.commit()
+    elif scope == 'SP':
+        print("don't support this scope function yet")
+        # start_time =
+        # end_time = today
+        # etl = daily_etl(interval, start_time, end_time)
+    else:
+        print("wrong scope")
 
-
-q12="CREATE TABLE ##Out_PFCodes(Parent_Fund_Code Fund_Code_Class)"
-cur12 = con.cursor()
-cur12.execute(q12)
-cur12.connection.commit()
-
-q13="""INSERT INTO ##Out_PFCodes
-SELECT DISTINCT Parent_Fund_Code 
-FROM ##Temp_Output
-WHERE Unit_Distr_Income IS NULL"""
-cur13 = con.cursor()
-cur13.execute(q13)
-cur13.connection.commit()
-
-q14 = """UPDATE ##Temp_Output SET Report = 'Y'"""
-# SET Report = 'Y' WHERE Parent_Fund_Code IN (Select Parent_Fund_Code FROM ##Out_PFCodes)"""
-cur14 = con.cursor()
-cur14.execute(q14)
-cur14.connection.commit()
-
-q15="""SELECT a.Parent_Fund_Code, a.Main_Fund_Code, b.Fund_Name, Unit_Distr_Cap, Unit_Distr_Income, b.T_Fund_Flag, b.Corp_Trust_Code, b.Fund_Type
-FROM ##Temp_Output a, Fund b
-WHERE Report = 'Y' AND
-a.Main_Fund_Code = b.Main_Fund_Code
-group by a.Parent_Fund_Code, a.Main_Fund_Code, b.Fund_Name, Unit_Distr_Cap, Unit_Distr_Income, 
-b.T_Fund_Flag, b.Corp_Trust_Code, b.Fund_Type
-ORDER BY a.Parent_Fund_Code"""
-cur15 = con.cursor()
-cur15.execute(q15)
+    if interval is not None:
+        etl = daily_etl(interval, start_time, end_time)
 
 
 
 
-
-def print_row(row):
-	row_string = ""
-	remaining_in_parent = 9 - len(str(row[0]).rstrip('\n'))
-	remaining_in_main = 9 - len(str(row[1]).rstrip('\n'))
-	#calculate the number of remaining spaces in the Fund_Name cell by subtracting the length of the current row's Fund_Name from the maximum Fund_Name length, which is 41 as of 08/14/2018. 
-	#Maximum Fund_Name length can be adjusted below later if Fund_Name lengths increase or decrease. The same applies to all other 'remaining_in' local variables
-	remaining_in_name_cell = 41 - len(str(row[2].encode('utf-8').rstrip('\n')))
-	if ('®' in str(row[2].encode('utf-8').rstrip('\n'))):
-		remaining_in_name_cell += 1
-	remaining_in_cap = 10 - len(str(row[3]).rstrip('\n'))
-	remaining_in_income = 10 - len(str(row[4]).rstrip('\n'))
-	remaining_in_tflag = 5 - len(str(row[5]))
-	remaining_in_corp = 6 - len(str(row[6]))
-	remaining_in_type = 10 - len(str(row[7]))
-	row_string += str(row[0]).rstrip('\n') + (" " * remaining_in_parent) + "," + str(row[1]).rstrip('\n') + (" " * remaining_in_main) + "," + str(row[2].encode('utf-8').rstrip('\n')) + (" " * remaining_in_name_cell) + "," + str(row[3]).rstrip('\n') + (" " * remaining_in_cap) +  "," + \
-	str(row[4]).rstrip('\n') + (" " * remaining_in_income) + ","  + str(row[5]).rstrip('\n') + (" " * remaining_in_tflag) +  "," + str(row[6]).rstrip('\n') + (" " * remaining_in_corp) + "," + \
-	str(row[7]).rstrip('\n') + (" " * remaining_in_type)
-	return row_string
-
-rows = cur15.fetchall()
-rows_written = 0
-for row in rows:
-	row = list(row)
-	if row[0] == None:
-		row[0] = "No Code"
-	if row[1] == None:
-		row[1] = "No Code"
-	if row[2] == None:
-		row[2] = "No Name"
-	if row[3] == None:
-		row[3] = "No Data"
-	if row[4] == None:
-		row[4] = "No Data"
-	if row[5] == None:
-		row[5] = "No Data"
-	if row[6] == None:
-		row[6] = "No Data"
-	if row[7] == None:
-		row[7] = "No Data"
-	
-	f.write(print_row(row))
-	f.write("\n")
-	rows_written += 1
-	row = tuple(row)
-
-if (rows_written == 0):
-	f.write("\nNo Distributions for " + str(today_date.strftime('%m-%d-%y')))
-
-f.close()
-con.close()
--- python_py/project_utils/db_util.py
+++ python_py/project_utils/db_util.py
@@ -4,6 +4,8 @@ import pandas.io.sql as pdsqlio
 from python_py.project_utils.config import DB_CONFIG
 from python_py.project_utils.config import API_KEY_FINN
 import finnhub
+from sqlalchemy import create_engine
+import os
 
 
 def connect_to_db():
@@ -20,10 +22,48 @@ def connect_to_db():
     )
     return db_conn
 
+
+def create_db_engine():
+    """
+    initiate an pandas to_sql db engine
+    :return: db engine object
+    """
+    database = ''.join(DB_CONFIG["DATABASE"])
+    user = DB_CONFIG["USER"]
+    password = DB_CONFIG["PASSWORD"]
+    host = DB_CONFIG["HOST"]
+    port = DB_CONFIG["PORT"]
+
+    # database = ''.join(DB_CONFIG["DATABASE"])
+    # user = os.getenv("USER")
+    # password = os.getenv("PASSWORD")
+    # host = os.getenv("HOST")
+    # port = os.getenv("PORT")
+#    url_eg = f'postgresql://user:{password}@{host}:{port}/{database}'
+    url_eg = "postgresql://" + user + ":" + password + "@" + host + ":" + port + "/" + database
+    print(url_eg)
+#    db_engine = create_engine('postgresql://scott:tiger@localhost/mydatabase')
+    db_engine = create_engine(url_eg, pool_size=10, max_overflow=20)
+#        driver='postgresql://',
+#        username=DB_CONFIG["USER"],
+#        password=DB_CONFIG["PASSWORD"],
+#        host="@"+DB_CONFIG["HOST"],
+#        port=DB_CONFIG["PORT"],
+#        database=DB_CONFIG["DATABASE"]
+#    )
+
+    return db_engine
+
+
+#    engine = create_engine('postgresql://', echo=False)
+
+
 # test db connection
 # db = connect_to_db()
+# engine = create_engine()
 # cursor = db.cursor
-# sql = "select * from us_equity_daily_finnhub"
+# internal = "daily"
+# sql = f"select * from us_equity_{internal}_finnhub"
 # sql = "select * from sales"
 # df = pdsqlio.read_sql_query(sql, db)
 # print(df.head())
@@ -31,17 +71,24 @@ def connect_to_db():
 
 def download_data_from_finnhub(security_symbol, interval, start_time, end_time):
     api_key_finn = API_KEY_FINN["API_KEY_FINN"]
-#    finnhub_client = finnhub.Client(api_key="bvrpbjf48v6ol3okodkg")
+    #    finnhub_client = finnhub.Client(api_key="bvrpbjf48v6ol3okodkg")
     finnhub_client = finnhub.Client(api_key=api_key_finn)
     try:
-        return finnhub_client.stock_candles(security_symbol, interval, start_time, end_time)
+        raw_data = finnhub_client.stock_candles(security_symbol, interval, start_time, end_time)
+        raw_data_df = pd.DataFrame(data=raw_data)
+        return raw_data_df
     except Exception as e:
         print(e, security_symbol, "api fetching failed")
         return None
 
 
-raw_data = download_data_from_finnhub('AAPL', 'D', 1590988249, 1591852249)
-df = pd.DataFrame(data=raw_data)
-print(df.head())
+# test download_data_from_finnhub
+# df = download_data_from_finnhub('AAPL', 'D', 1590988249, 1591852249)
+# print(df.head())
+
+def datetime_to_int(dt):
+    return int(dt.strftime("%Y%m%d%H%M%S"))
 
 
+def datetime_to_int_todate(dt):
+    return int(dt.strftime("%Y%m%d"))
